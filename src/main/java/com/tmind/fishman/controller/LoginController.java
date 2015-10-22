9bc124f52dc4cbf88a8d77758223bb7d10ff4c1c
vani_junying@hotmail.com <vali@SSC1-W257.schenker-asia.com>
2015-10-22 15:49:59 +0800

    update login function

--- src/main/java/com/tmind/fishman/controller/LoginController.java
+++ src/main/java/com/tmind/fishman/controller/LoginController.java
@@ -6,9 +6,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.tmind.fishman.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 /*
@@ -19,17 +23,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
 	// Standard JSR250 Injection
-
+    Logger log = LoggerFactory.getLogger(LoginController.class);
 
 	@RequestMapping(params = "weblogin")
-	public String login(@ModelAttribute("user") User user,
+	public ModelAndView login(@ModelAttribute("user") User user,
 			HttpServletRequest req, HttpServletResponse response) {
-		System.out.println("用户信息: " + user);
+        log.info(user.getUserId());
 		// 传递对象于下一页面
 		// TODO check database to get user, for now just use 'admin' 'admin' for
 		// mock
-
-		return "login";
+        ModelMap model = new ModelMap();
+        model.addAttribute("userId", user.getUserId());
+		return new ModelAndView("CreateRoom", model);
 	}
 
 
